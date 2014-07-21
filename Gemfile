e040944da9d37a4db20ae4a0ff0adc880a0bc6e4
Leonardo Balter <leonardo.balter@gmail.com>
2014-07-21 17:59:05 -0300

    First conceptual use of i18n on nanoc

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem "nanoc", "~> 3.7.1"
+gem "i18n"
--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,9 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+require 'i18n'
+
+I18n.load_path = ['locale/en.yml', 'locale/pt.yml']
+
+# Default to pt
+I18n.locale = :pt
