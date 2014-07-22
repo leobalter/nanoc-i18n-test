eb55de6ffae3a95da4bb5396138ce3a5c8c14015
Nando Vieira <fnando.vieira@gmail.com>
2014-07-22 17:40:39 -0300

    Enable locale fallback.

--- lib/locale.rb
+++ lib/locale.rb
@@ -1,6 +1,8 @@
 require 'i18n'
+require 'i18n/backend/fallbacks'
 
 I18n.enforce_available_locales = false
+I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 I18n.load_path += Dir[File.expand_path('../../locale/**/*.yml', __FILE__)]
 I18n.default_locale = :en
 I18n.available_locales = %w[ en pt ]
