87f2d978596d1013c6537f2d6e8cde1690bdb233
Nando Vieira <fnando.vieira@gmail.com>
2014-07-22 17:37:57 -0300

    Set locale for item, using the dirname as identifier.

--- lib/locale.rb
+++ lib/locale.rb
@@ -1,20 +1,12 @@
 require 'i18n'
 
-LOCALES = {
-  'en' => 'locale/en.yml',
-  'pt' => 'locale/pt.yml',
-  'pt-br' => 'locale/pt.yml'
-};
-
-I18n.load_path = LOCALES.values
-
-# Removing stuppid deprecated msg with this command below
 I18n.enforce_available_locales = false
+I18n.load_path += Dir[File.expand_path('../../locale/**/*.yml', __FILE__)]
+I18n.default_locale = :en
+I18n.available_locales = %w[ en pt ]
 
-def locale(loc)
-  loc = loc.split('/')[1]
-  if ! LOCALES.has_key?(loc)
-    loc = :en
-  end
-  I18n.locale = loc
+def set_locale_for_item(item)
+  _, locale, _ = item.raw_filename.split('/')
+  I18n.locale = :en
+  I18n.locale = locale if I18n.available_locales.include?(locale.to_sym)
 end
